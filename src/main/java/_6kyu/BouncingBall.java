1a3832584cee9cf058887b4ddf1a9c00ce105ef4
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-04-10 10:16:03 +0300

    6 kyu Bouncing Balls solution and testing

---
+++ src/main/java/_6kyu/BouncingBall.java
@@ -0,0 +1,17 @@
+package _6kyu;
+
+public class BouncingBall {
+
+    public static int bouncingBall(double h, double bounce, double window) {
+        if ((h <= 0) || (bounce <= 0) || (bounce >= 1) || (window >= h)) return -1;
+        int count = 0;
+        while (h > window) {
+            count += 1;
+            h = h * bounce;
+            if (h > window) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
