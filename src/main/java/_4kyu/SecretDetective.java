430608f285879e879855b4d8fced03691a7176a1
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-05-16 23:20:42 +0300

    4 kyu Recover a secret string from random triplets debag test2

--- src/main/java/_4kyu/SecretDetective.java
+++ src/main/java/_4kyu/SecretDetective.java
@@ -10,37 +10,49 @@ public class SecretDetective {
         for (Character character : triplets[0]) {
             resultList.add(character);
         }
+
         List<List<Character>> tripletsList = new ArrayList<>();        //transform array to list without first line
         for (int i = 1; i < triplets.length; i++) {
             List<Character> tripletsLine = new ArrayList<>();
             for (Character character : triplets[i]) {
                 tripletsLine.add(character);
             }
+            System.out.println(tripletsLine); //for test
             tripletsList.add(tripletsLine);
         }
         while (!tripletsList.isEmpty()) {
             List<List<Character>> tempList = new ArrayList<>();
             for (List<Character> line : tripletsList) {
                 boolean isActive = false;
-                if (line == null || line.isEmpty() || line.size() == 1) {
-                    break;
+                if (line == null || line.isEmpty() || line.size() == 1 || resultList.containsAll(line)) {
+                    continue;
                 }
                 for (Character letter : line) {
                     if (letter.equals(resultList.get(resultList.size() - 1)) && line.indexOf(letter) < line.size() - 1) {
                         for (int i = line.indexOf(letter) + 1; i < line.size(); i++) {
                             resultList.add(line.get(i));
                         }
+                        List<Character> subLineStart = new ArrayList<>();
+                        for (int i = 0; i < line.indexOf(letter) + 1; i++) {
+                            subLineStart.add(line.get(i));
+                        }
+                        tempList.add(subLineStart);
                         isActive = true;
                         break;
                     } else if (resultList.get(0).equals(letter) && line.indexOf(letter) != 0) {
                         for (int i = line.indexOf(letter) - 1; i >= 0; i--) {
                             resultList.add(0, line.get(i));
                         }
+                        List<Character> subLineEnd = new ArrayList<>();
+                        for (int i = line.indexOf(letter); i < line.size(); i++) {
+                            subLineEnd.add(line.get(i));
+                        }
+                        tempList.add(subLineEnd);
                         isActive = true;
                         break;
                     }
                 }
-                if (isActive || resultList.containsAll(line)) {
+                if (isActive ) {
                 } else if (line.size() == 2) {
                     tempList.add(line);
                 } else if (resultList.contains(line.get(0)) && resultList.contains(line.get(2)) &&
--- src/test/java/_4kyu/SecretRecoveryTest.java
+++ src/test/java/_4kyu/SecretRecoveryTest.java
@@ -14,13 +14,22 @@ public class SecretRecoveryTest {
 
     @Test public void secret1() {
         char[][] triplets = {
-                {'t','u','p'},
-                {'w','h','i'},
-                {'t','s','u'},
-                {'a','t','s'},
-                {'h','a','p'},
-                {'t','i','s'},
-                {'w','h','s'}
+                {'a','s','u'},
+                {'m','a','f'},
+                {'a','i','n'},
+                {'s','u','n'},
+                {'m','f','u'},
+                {'a','t','h'},
+                {'t','h','i'},
+                {'h','i','f'},
+                {'m','h','f'},
+                {'a','u','n'},
+                {'m','a','t'},
+                {'f','u','n'},
+                {'h','s','n'},
+                {'a','i','s'},
+                {'m','s','n'},
+                {'m','s','u'}
         };
         assertEquals("whatisup", detective.recoverSecret(triplets));
     }
