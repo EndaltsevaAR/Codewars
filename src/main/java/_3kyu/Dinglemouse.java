f9ee5637aa59a847aa779efa39b5ebedff7bea8e
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-06-28 00:17:55 +0300

    3 kyu The Lift kata solving and testing

--- src/main/java/_3kyu/Dinglemouse.java
+++ src/main/java/_3kyu/Dinglemouse.java
@@ -1,44 +1,13 @@
 package _3kyu;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Dinglemouse {
     static List<Integer> liftMovingList; //result list of moving list
     static List<Integer> floorsToStopList;  //list of current passengers
 
-    public static void main(String[] args) {
-        final int[][] queues = {
-                new int[]{9,3,8}, // G
-                new int[]{10,11}, // 1
-                new int[0], // 2
-                new int[]{0,4,16}, // 3
-                new int[]{15,14,3}, // 4
-                new int[0], // 5
-                new int[]{3,15,3,10}, // 6
-                new int[]{16,14,12,15}, // 7
-                new int[0], // 8
-                new int[]{15,1}, // 9
-                new int[]{0,6,13,8}, // 10
-                new int[]{10}, // 11
-                new int[]{10}, // 12
-                new int[]{7,15,1,6}, // 13
-                new int[]{2,1,9}, // 14
-                new int[]{4,10,2,13}, // 15
-                new int[]{6,3,2,15}, // 16
-        };
-        System.out.println(Arrays.toString(theLift(queues, 4)));
-    }
-
-
     public static int[] theLift(final int[][] queues, final int capacity) {
-        System.out.println("capacity");
-        for (int[] floor :queues) {
-            System.out.println(Arrays.toString(floor));
-        }
-        System.out.println();
-
         liftMovingList = new ArrayList<>(); //result list of moving list
         floorsToStopList = new ArrayList<>();  //list of current passengers
         liftMovingList.add(0); //start always from ground
@@ -54,8 +23,8 @@ public class Dinglemouse {
             for (int floor = currentFloor; floor > 0; floor--) {    //moving to ground
                 entryFloorChecking(false, floor, queues, capacity);
             }
-
         }
+
         if (liftMovingList.get(liftMovingList.size() - 1) != 0) {
             liftMovingList.add(0); //end always at the ground
         }
@@ -64,7 +33,7 @@ public class Dinglemouse {
     }
 
     private static void entryFloorChecking(boolean isUp, int floor, final int[][] queues, int capasity) {
-        if (floorsToStopList.contains(floor)) {
+        if (floorsToStopList.contains(floor)) {  //passenger unloading
             floorsToStopList = floorsToStopListWithoutThisFloor(floorsToStopList, floor);
             if (liftMovingList.get(liftMovingList.size() - 1) != floor) {
                 liftMovingList.add(floor);
@@ -72,11 +41,13 @@ public class Dinglemouse {
         }
 
         for (int pass = 0; pass < queues[floor].length; pass++) {
-            if ( queues[floor][pass] != -1 && ((queues[floor][pass] > floor && isUp) || (queues[floor][pass] < floor && !isUp)) && floorsToStopList.size() < capasity) {
-                floorsToStopList.add(queues[floor][pass]);
-                queues[floor][pass] = -1; //if passengers use lift, his place at the queue is reset to 0
+            if ( queues[floor][pass] != -1 && ((queues[floor][pass] > floor && isUp) || (queues[floor][pass] < floor && !isUp))) {
+                if (floorsToStopList.size() < capasity) {   //if lift has free sits, passenger can enter
+                    floorsToStopList.add(queues[floor][pass]);
+                    queues[floor][pass] = -1; //if passengers use lift, his place at the queue is reset to 0
+                }
                 if (liftMovingList.get(liftMovingList.size() - 1) != floor) {  //to avoid duplicates for many passengers from one floor
-                    liftMovingList.add(floor);
+                    liftMovingList.add(floor);  //enter passenger or not, but lift stopping in counted
                 }
             }
         }
