6aa1459b60583ed7248c4d06ec5c72d83b8c2845
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-07-06 00:47:35 +0300

    3 kyu Screen Locking Patterns's solution before testing

--- src/main/java/_3kyu/ScreenLock.java
+++ src/main/java/_3kyu/ScreenLock.java
@@ -5,25 +5,72 @@ import java.util.List;
 
 public class ScreenLock {
     final static char[][] keyboard = new char[][]{{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}};
-    public int calculateCombinations(char startPosition, int patternLength){
 
-        List<List<Character>> resultList = new ArrayList<>();
-        List<Character> first = new ArrayList<>();
-        first.add(startPosition);
-        resultList.add(first);
+    public static void main(String[] args) {
+        ScreenLock screenLock = new ScreenLock();
+        System.out.println(screenLock.calculateCombinations('C', 2));
+    }
+
+    public int calculateCombinations(char startPosition, int patternLength) {
+        List<String> resultList = new ArrayList<>();
+        resultList.add(Character.toString(startPosition));
         for (int i = 0; i < patternLength; i++) {
             resultList = findNeighbor(resultList);
         }
-        return 0;
+        return resultList.size();
     }
 
-    private List<List<Character>> findNeighbor(List<List<Character>> resultList) {
-        List<List<Character>> stepList = new ArrayList<>();
-        for (List<Character> branch: resultList) {
-            List<Integer> lastLetterCoordinates = findCoordinates(branch.get(branch.size() - 1)); //last letter
-            
+    private List<String> findNeighbor(List<String> resultList) {
+        List<String> stepList = new ArrayList<>();  //поменять на стрингбилдеры
+        for (String branch : resultList) {
+            for (int y = 0; y < keyboard.length; y++) {
+                for (int x = 0; x < keyboard[0].length; x++) {
+                    List<Integer> lastCoordinates = findCoordinates(branch.charAt(branch.length() - 1));
+                    if (!branch.contains(Character.toString(keyboard[y][x])) && !isHaveOpenNeighbors(lastCoordinates.get(0), lastCoordinates.get(1), y, x, branch)) {
+                        stepList.add(branch + keyboard[y][x]);
+                    }
+                }
+            }
+        }
+        return stepList;
+    }
+
+    private boolean isHaveOpenNeighbors(int yFirst, int xFirst, int yNewLetter, int xNewLetter, String branch) {
+        List<Integer> yCoordinates = new ArrayList<>();
+        List<Integer> xCoordinates = new ArrayList<>();
+        boolean isYMove = false;
+        boolean isXMove = false;
+        if ((yNewLetter - yFirst) % 2 == 0 && yNewLetter != yFirst) {
+            if ((xNewLetter - xFirst) % 2 == 0 && xNewLetter != xFirst) {
+                isYMove = true;
+                isXMove = true;
+            } else {
+                isYMove = true;
+            }
+        } else {
+            isXMove = true;
         }
 
+        if (isYMove) {
+            for (int i = Math.min(yFirst, yNewLetter) + 1; i < Math.max(yFirst, yNewLetter); i++) {
+                yCoordinates.add(i);
+            }
+        }
+        if (isXMove) {
+            for (int i = Math.min(xFirst, xNewLetter) + 1; i < Math.max(xFirst, xNewLetter); i++) {
+                xCoordinates.add(i);
+            }
+        }
+
+
+        for (int i = 0; i < Math.max(yCoordinates.size(), xCoordinates.size()); i++) {
+            if (isYMove && isXMove) {
+                if (!branch.contains(Character.toString(keyboard[yCoordinates.get(i)][xCoordinates.get(i)]))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     private static List<Integer> findCoordinates(char startPosition) {
