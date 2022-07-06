8ca1842af4da34be536c97099e5257cae6c3781f
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-07-07 00:08:16 +0300

    3 kyu Screen Locking Patterns's final solution and testing

--- src/main/java/_3kyu/ScreenLock.java
+++ src/main/java/_3kyu/ScreenLock.java
@@ -1,10 +1,52 @@
 package _3kyu;
 
+/*
+Description:
+Screen Locking Patterns
+You might already be familiar with many smartphones that allow you to use a geometric pattern as a security measure. To unlock the device, you need to connect a sequence of dots/points in a grid by swiping your finger without lifting it as you trace the pattern through the screen.
+
+The image below has an example pattern of 7 dots/points: (A -> B -> I -> E -> D -> G -> C).
+
+lock_example.png
+
+For this kata, your job is to implement a function that returns the number of possible patterns starting from a given first point, that have a given length.
+
+More specifically, for a function countPatternsFrom(firstPoint, length), the parameter firstPoint is a single-character string corresponding to the point in the grid (i.e.: 'A') where your patterns start, and the parameter length is an integer indicating the number of points (length) every pattern must have.
+
+For example, countPatternsFrom("C", 2), should return the number of patterns starting from 'C' that have 2 two points. The return value in this case would be 5, because there are 5 possible patterns:
+
+(C -> B), (C -> D), (C -> E), (C -> F) and (C -> H).
+
+Bear in mind that this kata requires returning the number of patterns, not the patterns themselves, so you only need to count them. Also, the name of the function might be different depending on the programming language used, but the idea remains the same.
+
+Rules
+In a pattern, the dots/points cannot be repeated: they can only be used once, at most.
+In a pattern, any two subsequent dots/points can only be connected with direct straight lines in either of these ways:
+Horizontally: like (A -> B) in the example pattern image.
+Vertically: like (D -> G) in the example pattern image.
+Diagonally: like (I -> E), as well as (B -> I), in the example pattern image.
+Passing over a point between them that has already been 'used': like (G -> C) passing over E, in the example pattern
+image. This is the trickiest rule. Normally, you wouldn't be able to connect G to C, because E is between them, however
+when E has already been used as part the pattern you are tracing, you can connect G to C passing over E, because E is
+ignored, as it was already used once.
+
+The sample tests have some examples of the number of combinations for some cases to help you check your code.
+
+Haskell Note: A data type Vertex is provided in place of the single-character strings. See the solution setup code for
+more details.
+
+Fun fact:
+
+In case you're wondering out of curiosity, for the Android lock screen, the valid patterns must have between 4 and 9
+dots/points. There are 389112 possible valid patterns in total; that is, patterns with a length between 4 and 9
+dots/points.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScreenLock {
-    final static char[][] keyboard = new char[][]{{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}};
+    final static char[][] keyboard = new char[][]{{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}};       //keyboard of smartphone
 
     public static void main(String[] args) {
         ScreenLock screenLock = new ScreenLock();
@@ -12,28 +54,28 @@ public class ScreenLock {
     }
 
     public int calculateCombinations(char startPosition, int patternLength) {
-        if (patternLength == 0) return 0;
-        List<String> resultList = new ArrayList<>();
-        resultList.add(Character.toString(startPosition));
+        if (patternLength == 0) return 0;     //there is empty list
+        List<List<Character>> resultList = new ArrayList<>();    //list of combinations
+        List<Character> startList = new ArrayList<>();  //first list (just first letter)
+        startList.add(startPosition);
+        resultList.add(startList);
         for (int i = 1; i < patternLength; i++) {
-            resultList = findNeighbor(resultList);
+            resultList = findNeighbor(resultList); //dynamic programming: result list is overwritten and expand each step
         }
-        for (String s :resultList) {
-            System.out.print(s + " ");
-        }
-
         return resultList.size();
     }
 
-    private List<String> findNeighbor(List<String> resultList) {
-        List<String> stepList = new ArrayList<>();  //поменять на стрингбилдеры
-        for (String branch : resultList) {
-            List<Integer> lastCoordinates = findCoordinates(branch.charAt(branch.length() - 1));
+    private List<List<Character>> findNeighbor(List<List<Character>> resultList) {
+        List<List<Character>> stepList = new ArrayList<>(); //combinations of this step
+        for (List<Character> branch : resultList) { //for each exist combination
+            List<Integer> lastCoordinates = findCoordinates(branch.get(branch.size() - 1)); //coordinations of last element current combination
             for (int y = 0; y < keyboard.length; y++) {
                 for (int x = 0; x < keyboard[0].length; x++) {
-                    if (!branch.contains(Character.toString(keyboard[y][x])) &&
-                            !isHaveOpenNeighbors(lastCoordinates.get(0), lastCoordinates.get(1), y, x, branch)) {
-                        stepList.add(branch + keyboard[y][x]);
+                    if (!branch.contains(keyboard[y][x]) && //if this letter already in current combination, there is mistake
+                            !isHaveOpenNeighbors(lastCoordinates.get(0), lastCoordinates.get(1), y, x, branch)) { //we can not go throw not used letters
+                        List<Character> temp = new ArrayList<>(branch);
+                        temp.add(keyboard[y][x]);
+                        stepList.add(temp);
                     }
                 }
             }
@@ -41,46 +83,48 @@ public class ScreenLock {
         return stepList;
     }
 
-    private boolean isHaveOpenNeighbors(int yFirst, int xFirst, int yNewLetter, int xNewLetter, String branch) {
+    private boolean isHaveOpenNeighbors(int yFirst, int xFirst, int yNewLetter, int xNewLetter, List<Character> branch) {
         List<Integer> yCoordinates = new ArrayList<>();
         List<Integer> xCoordinates = new ArrayList<>();
         boolean isYMove = false;
         boolean isXMove = false;
-        if ((Math.abs(yNewLetter - yFirst)) % 2 == 0 && (Math.abs(xNewLetter - xFirst))% 2 != 1 && yNewLetter != yFirst) {
-            if ((Math.abs(xNewLetter - xFirst)) % 2 == 0 && xNewLetter != xFirst ) {
+        //we define definition of possible moving
+        if ((Math.abs(yNewLetter - yFirst)) % 2 == 0 && (Math.abs(xNewLetter - xFirst))% 2 != 1 && yNewLetter != yFirst) {  //moving OY
+            if ((Math.abs(xNewLetter - xFirst)) % 2 == 0 && xNewLetter != xFirst ) { //with moving OX
                 isYMove = true;
                 isXMove = true;
-            } else {
+            } else { //without moving OX
                 isYMove = true;
             }
-        } else if ((Math.abs(xNewLetter - xFirst)) % 2 == 0 && yNewLetter == yFirst && xNewLetter != xFirst) {
+        } else if ((Math.abs(xNewLetter - xFirst)) % 2 == 0 && yNewLetter == yFirst && xNewLetter != xFirst) { //moving just OX
             isXMove = true;
 
         }
-
+        // if there is need moving at the OY coordinate
         if (isYMove) {
             for (int i = Math.min(yFirst, yNewLetter) + 1; i < Math.max(yFirst, yNewLetter); i++) {
                 yCoordinates.add(i);
             }
         }
+        // if there is need moving at the OX coordinate
         if (isXMove) {
             for (int i = Math.min(xFirst, xNewLetter) + 1; i < Math.max(xFirst, xNewLetter); i++) {
                 xCoordinates.add(i);
             }
         }
 
-
+        // we compare every cell that is on the road with the current combination. If we found not used one, we can not use this road
         for (int i = 0; i < Math.max(yCoordinates.size(), xCoordinates.size()); i++) {
             if (isYMove && isXMove) {
-                if (!branch.contains(Character.toString(keyboard[yCoordinates.get(i)][xCoordinates.get(i)]))) {
+                if (!branch.contains(keyboard[yCoordinates.get(i)][xCoordinates.get(i)])) {
                     return true;
                 }
             } else if (isYMove) {
-                if (!branch.contains(Character.toString(keyboard[yCoordinates.get(i)][xFirst]))) {
+                if (!branch.contains(keyboard[yCoordinates.get(i)][xFirst])) {
                     return true;
                 }
             } else if (isXMove) {
-                if (!branch.contains(Character.toString(keyboard[yFirst][xCoordinates.get(i)]))) {
+                if (!branch.contains(keyboard[yFirst][xCoordinates.get(i)])) {
                     return true;
                 }
             }
--- src/test/java/_3kyu/ScreenLockTest.java
+++ src/test/java/_3kyu/ScreenLockTest.java
@@ -1,19 +1,143 @@
 package _3kyu;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
+import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScreenLockTest {
     ScreenLock sl = new ScreenLock();
 
     @Test
-    public void bascis() {
-        assertEquals(0, sl.calculateCombinations('A', 10));
-        assertEquals(0, sl.calculateCombinations('A', 0));
-        assertEquals(0, sl.calculateCombinations('E', 14));
-        assertEquals(1, sl.calculateCombinations('B', 1));
-        assertEquals(5, sl.calculateCombinations('C', 2));
-        assertEquals(8, sl.calculateCombinations('E', 2));
-        assertEquals(256, sl.calculateCombinations('E', 4));
+    public void bascisInputs (){
+
+        assertEquals(0, sl.calculateCombinations('A',10));
+        assertEquals(0, sl.calculateCombinations('A',0));
+        assertEquals(0, sl.calculateCombinations('E',14));
+        assertEquals(1, sl.calculateCombinations('B',1));
+        assertEquals(5, sl.calculateCombinations('C',2));
+        assertEquals(8, sl.calculateCombinations('E',2));
     }
+
+    @Test
+    public void someInputs (){
+        assertEquals(256, sl.calculateCombinations('E',4));
+        assertEquals(37, sl.calculateCombinations('D',3));
+    }
+
+    @Test
+    public void largeInputs (){
+        assertEquals(23280, sl.calculateCombinations('E',8));
+    }
+
+    @Test
+    public void randomTests(){
+        Random rnd = new Random();
+        MyScreenLock my = new MyScreenLock();
+        char[] letters = {'A','B','C','D','E','F','G','H','I','B'};
+        for(int i=0; i<10; i++){
+            int patternLength = (int)(rnd.nextDouble() * 10.0);
+            assertEquals(my.calculateCombinations(letters[patternLength],patternLength),
+                    sl.calculateCombinations(letters[patternLength],patternLength));
+        }
+
+    }
+
+    private class MyScreenLock {
+
+        private String LETTERS = "ABCDEFGHI";
+        private final List<String> notAllowedPatterns = Arrays.asList("AC,AG,AI,BH,CI,CA,CG,DF,FD,GA,GC,GI,HB,IC,IG,IA".split(","));
+        private final Map<String, String> map = new HashMap<>();
+        public void init(){
+            map.put("AC","B");
+            map.put("AG","D");
+            map.put("AI","E");
+            map.put("BH","E");
+            map.put("CI","F");
+            map.put("CG","E");
+            map.put("DF","E");
+            map.put("GI","H");
+        }
+        public int calculateCombinations(char startPosition, int patternLength){
+            if(patternLength>LETTERS.length()) return 0; //no combination
+            if(patternLength==0) return 0; //zero combinations
+            init();
+            String chain = "";//startPosition+"";
+            for (int i = 0; i < LETTERS.length(); i++) {
+                chain += (LETTERS.charAt(i) == startPosition) ? "" : LETTERS.charAt(i);
+            }
+
+            ArrayList<String> s = permutate(chain);
+            Set<String> hashSet = s.stream().map(str->startPosition+str.substring(0, patternLength-1)).collect(Collectors.toSet());
+
+            int count = 0;
+            Iterator it = hashSet.iterator();
+            while (it.hasNext()){
+                String w = (String)it.next();
+                boolean pass = true;
+                for (int i = 0; i < notAllowedPatterns.size(); i++) {
+                    if(w.contains(notAllowedPatterns.get(i))){
+                        if(w.startsWith(notAllowedPatterns.get(i))){
+                            pass = false;
+                            break;
+                        }
+                        String character;
+                        String pattern = notAllowedPatterns.get(i);
+                        if(map.get(pattern) == null){
+                            character = map.get(pattern.charAt(1)+""+pattern.charAt(0));
+                        }else{
+                            character = map.get(pattern);
+                        }
+
+                        if(!w.substring(0,w.indexOf(pattern)).contains(character)){
+                            pass = false;
+                            break;
+                        }
+                    }
+                }
+                if(!pass) continue;
+                count++;
+            }
+            return count;
+        }
+
+        private ArrayList<String> permutate(String string){
+            ArrayList<String> s = permutater(string);
+            SortedMap<String, String> map = new TreeMap<>(String::compareTo);
+            for (int i = 0; i < s.size(); i++) {
+                map.put(s.get(i), "");
+            }
+            s.clear();
+            Iterator it = map.keySet().iterator();
+            while(it.hasNext()){
+                String key = (String)it.next();
+                s.add(key);
+            }
+            return s;
+        }
+        private ArrayList<String> permutater(String string){
+
+            ArrayList<String> words = new ArrayList<>();
+            if(string.length() == 1){
+                words.add(string);
+                return words;
+            }
+            for (int i = 0; i < string.length(); i++) {
+                String w = "";
+                for (int j = 0; j < string.length(); j++) {
+                    if(i==j) continue;
+                    w += string.charAt(j);
+                }
+                ArrayList<String> my = permutater(w);
+                for (int j = 0; j < my.size(); j++) {
+                    String toAdd = string.charAt(i)+""+my.get(j);
+                    words.add(toAdd);
+                }
+                if(my.get(0).length() == 8) break;
+            }
+            return words;
+        }
+    }
+
 }
