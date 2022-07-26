f763720762af14dbc05e80755d2fac6e3d3ef461
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-07-26 14:53:12 +0300

    4 kyu Finder solution with array instead of list to maze

--- src/main/java/_4kyu/Finder.java
+++ src/main/java/_4kyu/Finder.java
@@ -1,25 +1,55 @@
 package _4kyu;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Finder {
+    public static void main(String[] args) {
+        String a = "..W.WW....WWW.W......\n" +
+                ".WW...W.....WW.W.W.W.\n" +
+                ".........W......W....\n" +
+                "..W.....W...WW.W..W..\n" +
+                "W.....W....W....WW...\n" +
+                ".WW.W.....W.W.W....W.\n" +
+                "W........WW...WWWW...\n" +
+                "WWW...W....W.....WWW.\n" +
+                "....WW....WW......WW.\n" +
+                ".W....W..W.W.........\n" +
+                "WW.....WW.W..........\n" +
+                "WW..WW.......W...WW.W\n" +
+                "W....W...WWWW..W.WW.W\n" +
+                ".WW.WW......WW.......\n" +
+                "W.WWW.........WWWW...\n" +
+                ".WW.W..W.W.W.......W.\n" +
+                "..W.....W...W....WWW.\n" +
+                ".W....W...WW....W.W.W\n" +
+                "...WW..W.WW.WWW..W...\n" +
+                "..W.W.W..W.W..WW.WW..\n" +
+                "...WWWWWW....W.W..WW.";
+        System.out.println(pathFinder(a));
+    }
     static Queue<Node> queueForChecking;
     static List<Node> checkedNodesList;
-    static List<List<String>> mazeList;
+    static char[][] mazeArray;
 
     static boolean pathFinder(String maze) {
-        mazeList = new ArrayList<>();
-        for (String line : maze.split("\n")) {
-            mazeList.add(Arrays.stream(line.split("")).collect(Collectors.toList()));
+        String[] lines = maze.split("\n");
+        mazeArray = new char[lines.length][lines[0].split("").length];
+        for (int y = 0; y < lines.length; y++) {
+            for (int x = 0; x < lines[y].length(); x++) {
+                mazeArray[y][x] = lines[y].charAt(x);
+            }
         }
+
 
         queueForChecking = new LinkedList<>();
         checkedNodesList = new ArrayList<>();
 
         Node node = new Node(0,0);
+        if (isFinish(node)) return true;
         List<Node> neighbours = findNeighbours(node);
         queueForChecking.addAll(neighbours);
+        checkedNodesList.add(node);
+
 
         while (!queueForChecking.isEmpty()) {
             Node neighbour = queueForChecking.poll();
@@ -27,6 +57,7 @@ public class Finder {
                 return true;
             } else {
                 queueForChecking.addAll(findNeighbours(neighbour));
+                checkedNodesList.add(neighbour);
             }
         }
         return false;
@@ -35,26 +66,32 @@ public class Finder {
     private static List<Node> findNeighbours(Node node) {
         List<Node> nodes = new ArrayList<>();
         Node up = new Node(node.getY() - 1, node.getX());
-        if (up.getY() >= 0 && !checkedNodesList.contains(up)) {
+
+        if (up.getY() >= 0 && !checkedNodesList.contains(up) && isRoad(up)) {
             nodes.add(up);
         }
         Node down = new Node(node.getY() + 1, node.getX());
-        if (up.getY() < mazeList.size() && !checkedNodesList.contains(up)) {
+        if (down.getY() < mazeArray.length && !checkedNodesList.contains(down) && isRoad(down)) {
             nodes.add(down);
         }
         Node left = new Node(node.getY(), node.getX() - 1);
-        if (up.getX() >= 0 && !checkedNodesList.contains(up)) {
+
+        if (left.getX() >= 0 && !checkedNodesList.contains(left) && isRoad(left)) {
             nodes.add(left);
         }
         Node right = new Node(node.getY(), node.getX() + 1);
-        if (up.getX() < mazeList.get(0).size() && !checkedNodesList.contains(up)) {
+        if (right.getX() < mazeArray[0].length && !checkedNodesList.contains(right) && isRoad(right)) {
             nodes.add(right);
         }
         return nodes;
     }
 
     private static boolean isFinish(Node neighbour) {
-        return neighbour.getY() == mazeList.size() - 1 && neighbour.getX() == mazeList.get(0).size() - 1;
+        return neighbour.getY() == mazeArray.length - 1 && neighbour.getX() == mazeArray[0].length - 1;
+    }
+
+    private static boolean isRoad(Node neighbour) {
+        return mazeArray[neighbour.getY()][neighbour.getX()] == '.';
     }
 
 }
