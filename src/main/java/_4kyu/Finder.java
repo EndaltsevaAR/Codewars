bd3d3a5c2d89b7ac1801bf535403e346386a646b
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-07-28 16:31:31 +0300

    4 kyu Finder final solving by BFS algorithm

--- src/main/java/_4kyu/Finder.java
+++ src/main/java/_4kyu/Finder.java
@@ -1,22 +1,47 @@
 package _4kyu;
 
+/*
+Description:
+Task
+You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal directions (i.e. North, East, South, West). Return true if you can reach position [N-1, N-1] or false otherwise.
+
+Empty positions are marked ..
+Walls are marked W.
+Start and exit positions are empty in all test cases.
+ */
+
 import java.util.*;
 
 public class Finder {
-    public static void main(String[] args) {
-        String d = "......\n"+
-                "......\n"+
-                "......\n"+
-                "......\n"+
-                ".....W\n"+
-                "....W.";
-        System.out.println(pathFinder(d));
-    }
-    static Queue<Node> queueForChecking;
-    static List<Node> checkedNodesList;
-    static char[][] mazeArray;
+
+    static Queue<Node> queueForChecking; //queue for checking nodes by levels: first there are checked first element, then all his neighbours, than all neighbours of these neighbors, so on. Element is checked and  deleted from this queue
+    static int[][] checkedNodesArray; //array - a copy in size from the maze array, filled with 0 at the beginning. After checking the node, the array cell with the corresponding coordinates of the node is filled with 1. Used to check for duplication
+    static char[][] mazeArray; //transform string to char[][]
 
     static boolean pathFinder(String maze) {
+        transformStringMazeToArray(maze);
+        queueForChecking = new LinkedList<>();
+        checkedNodesArray = new int[mazeArray.length][mazeArray[0].length];
+
+        Node node = new Node(0,0); //start node (0,0)
+        if (isFinish(node)) return true; //if maze is just 1 cell
+        List<Node> neighbours = findNeighbours(node); //first node's neighbours
+        queueForChecking.addAll(neighbours);
+        checkedNodesArray[node.getY()][node.getX()] = 1;
+
+        while (!queueForChecking.isEmpty()) {      //BFS algorithm
+            Node neighbour = queueForChecking.poll();  //check neighbour
+            if (isFinish(neighbour)) {
+                return true;
+            } else {
+                queueForChecking.addAll(findNeighbours(neighbour)); //add all his neighbours to end of queue
+                checkedNodesArray[neighbour.getY()][neighbour.getX()] = 1; //current neighbour is checked
+            }
+        }
+        return false;
+    }
+
+    private static void transformStringMazeToArray(String maze) {
         String[] lines = maze.split("\n");
         mazeArray = new char[lines.length][lines[0].split("").length];
         for (int y = 0; y < lines.length; y++) {
@@ -24,48 +49,26 @@ public class Finder {
                 mazeArray[y][x] = lines[y].charAt(x);
             }
         }
-
-
-        queueForChecking = new LinkedList<>();
-        checkedNodesList = new ArrayList<>();
-
-        Node node = new Node(0,0);
-        if (isFinish(node)) return true;
-        List<Node> neighbours = findNeighbours(node);
-        queueForChecking.addAll(neighbours);
-        checkedNodesList.add(node);
-
-        int count = 0;
-        while (!queueForChecking.isEmpty()) {
-            Node neighbour = queueForChecking.poll();
-            if (isFinish(neighbour)) {
-                return true;
-            } else {
-                queueForChecking.addAll(findNeighbours(neighbour));
-                checkedNodesList.add(neighbour);
-            }
-        }
-        return false;
     }
 
     private static List<Node> findNeighbours(Node node) {
         List<Node> nodes = new ArrayList<>();
-        Node up = new Node(node.getY() - 1, node.getX());
-
-        if (up.getY() >= 0 && !checkedNodesList.contains(up) && isRoad(up) && !queueForChecking.contains(up)) {
+        Node up = new Node(node.getY() - 1, node.getX()); //for possible neighbours: up, down, left and right
+        //There are must be at the borders of maze array, and must be included at the checkedNodesArray and queueForChecking just once
+        if (up.getY() >= 0 && checkedNodesArray[up.getY()][up.getX()] == 0 && isRoad(up) && !queueForChecking.contains(up)) {
             nodes.add(up);
         }
         Node down = new Node(node.getY() + 1, node.getX());
-        if (down.getY() < mazeArray.length && !checkedNodesList.contains(down) && isRoad(down) && !queueForChecking.contains(down) ) {
+        if (down.getY() < mazeArray.length && checkedNodesArray[down.getY()][down.getX()] == 0 && isRoad(down) && !queueForChecking.contains(down) ) {
             nodes.add(down);
         }
         Node left = new Node(node.getY(), node.getX() - 1);
 
-        if (left.getX() >= 0 && !checkedNodesList.contains(left) && isRoad(left) && !queueForChecking.contains(left)) {
+        if (left.getX() >= 0 && checkedNodesArray[left.getY()][left.getX()] == 0 && isRoad(left) && !queueForChecking.contains(left)) {
             nodes.add(left);
         }
         Node right = new Node(node.getY(), node.getX() + 1);
-        if (right.getX() < mazeArray[0].length && !checkedNodesList.contains(right) && isRoad(right) && !queueForChecking.contains(right)) {
+        if (right.getX() < mazeArray[0].length && checkedNodesArray[right.getY()][right.getX()] == 0 && isRoad(right) && !queueForChecking.contains(right)) {
             nodes.add(right);
         }
         return nodes;
@@ -76,7 +79,7 @@ public class Finder {
     }
 
     private static boolean isRoad(Node neighbour) {
-        return mazeArray[neighbour.getY()][neighbour.getX()] == '.';
+        return mazeArray[neighbour.getY()][neighbour.getX()] == '.'; //'W' - is wall, '.' - is road
     }
 
 }
--- src/test/java/_4kyu/FinderTest.java
+++ src/test/java/_4kyu/FinderTest.java
@@ -1,14 +1,19 @@
 package _4kyu;
 
+import java.util.*;
+import java.util.stream.*;
+import java.awt.Point;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
 
-// TODO: Replace examples and use TDD development by writing your own tests
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FinderTest {
 
-    @Test public void sampleTests() {
+    @Test public void fixedTests() {
 
         String a = ".W.\n"+
                 ".W.\n"+
@@ -36,5 +41,148 @@ public class FinderTest {
         assertEquals(false, Finder.pathFinder(b));
         assertEquals(true,  Finder.pathFinder(c));
         assertEquals(false, Finder.pathFinder(d));
+    }
+
+
+    @Test public void moreTests() {
+
+        String a =
+                ".W...\n"+
+                        ".W...\n"+
+                        ".W.W.\n"+
+                        "...W.\n"+
+                        "...W.";
+        assertEquals(true, Finder.pathFinder(a));
+
+        a = ".W...\n"+
+                ".W...\n"+
+                ".W.W.\n"+
+                "...WW\n"+
+                "...W.";
+        assertEquals(false, Finder.pathFinder(a));
+
+        a = "..W\n"+
+                ".W.\n"+
+                "W..";
+        assertEquals(false, Finder.pathFinder(a));
+
+        a = ".WWWW\n"+
+                ".W...\n"+
+                ".W.W.\n"+
+                ".W.W.\n"+
+                "...W.";
+        assertEquals(true, Finder.pathFinder(a));
+
+        a = ".W...\n"+
+                "W....\n"+
+                ".....\n"+
+                ".....\n"+
+                ".....";
+        assertEquals(false, Finder.pathFinder(a));
+
+        a = ".W\n"+
+                "W.";
+        assertEquals(false, Finder.pathFinder(a));
+
+        a = ".";
+        assertEquals(true, Finder.pathFinder(a));
+    }
+
+
+
+
+    private static boolean DEBUG = false;
+
+    private Random rnd = new Random();
+
+    private int rand(int n)        { return rnd.nextInt(n); }
+    private int rand(int n, int m) { return n + rnd.nextInt(m-n+1); }
+
+    @Test
+    public void randomTests() {
+
+        int count = 0,
+                TIMES = 20,
+                RNG   = 100;
+
+        for (int nTimes=0 ; nTimes < TIMES ; nTimes++) {
+            for (int n=1 ; n < RNG+1 ; n++) {
+
+                String maze = generateMaze(n);
+
+                boolean expected = KikicestKiTrouveLaSolution.pathFinder(maze);
+                assertEquals(expected, Finder.pathFinder(maze));
+
+                count += expected ? 1:0;
+            }
+        }
+
+        if (DEBUG) System.out.println(""+count+"/"+(RNG*TIMES));
+    }
+
+
+    private String generateMaze(int n) {
+
+        int size   = n*n,
+                nWalls = rand(size/4, size/3);
+
+        Set<Integer> posWalls = new HashSet<>();
+        while (posWalls.size() < nWalls) posWalls.add(rand(size));
+
+        final StringBuilder[] sbArr = IntStream.range(0,n)
+                .mapToObj( x -> new StringBuilder( IntStream.range(0,n)
+                        .mapToObj( y -> "." )
+                        .collect(Collectors.joining()) ) )
+                .toArray(StringBuilder[]::new);
+
+        posWalls.forEach( w -> { int x = w/n, y = w%n;
+            sbArr[x].setCharAt(y,'W'); } );
+
+        sbArr[n-1].setCharAt(n-1, '.');        // first and last tile always free
+        sbArr[0].setCharAt(0, '.');
+
+        return Arrays.stream(sbArr)
+                .map( sb -> sb.toString() )
+                .collect(Collectors.joining("\n"));
+    }
+
+
+
+    private static final class KikicestKiTrouveLaSolution {
+
+        final private static List<Point> MOVES = Arrays.asList(new Point(1,0), new Point(0,1), new Point(0,-1), new Point(-1,0));
+
+
+        private static boolean pathFinder(String maze) {
+
+            int S = (int) Math.sqrt(maze.length()) - 1;
+            if (S == 0) return true;
+
+            final Set<Point> bag  = new HashSet<>();
+            int x = -1;
+            for (String line: maze.split("\n")) { x++;
+                for (int y=0 ; y < line.length() ; y++)
+                    if (line.charAt(y)=='.') bag.add(new Point(x,y));
+            }
+            bag.remove(new Point(0,0));
+
+            final Point     end    = new Point(S,S);
+            final boolean[] hasEnd = {false};
+            Set<Point>      look   = new HashSet<>(Arrays.asList(new Point(0,0)));
+
+            while (!look.isEmpty()) {
+                if (hasEnd[0]) return true;
+                look = look.stream()
+                        .flatMap( p -> MOVES.stream().map( d -> new Point(p.x+d.x, p.y+d.y) ))
+                        .distinct()
+                        .filter(  p -> { if (p.equals(end)) hasEnd[0] = true;
+                            if (bag.contains(p)) {
+                                bag.remove(p);
+                                return true;
+                            } else return false; })
+                        .collect(Collectors.toSet());
+            }
+            return false;
+        }
     }
 }
